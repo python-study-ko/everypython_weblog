fb66f3331c56ed9ec688ad12b91116edf95d78f2
wesky93 <wesky93@gmail.com>
2016-07-05 02:04:01 +0900

    카테고리 목록 추출 함수 추가
    카테고리 레벨별로 추출하도록 만들예정

--- webproject/blog/views.py
+++ webproject/blog/views.py
@@ -3,6 +3,13 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.base import TemplateView
 from blog.models import Category, Post
 # Create your views here.
+def category_level():
+    """
+    카테고리의 레벨별로 딕셔너리에 분류해주는 함수/ 카테고뢰 목록구현에 사용한다.
+    :return: {1:['a','b','c'],2:['d','e'],3:['x','z']}
+    """
+    for Category
+
 class Index(TemplateView):
     template_name ='blog/index.html'
 
