65512abbcd07cbb808b9cc04874fd04fe9fad31a
wesky93 <wesky93@gmail.com>
2016-07-05 16:18:29 +0900

    카테고리 구조를 딕셔너리에 담아주는 함수 추가
    해당 함수에서 생성된 딕셔너리를 템플릿에 넘겨준다.

--- webproject/blog/views.py
+++ webproject/blog/views.py
@@ -4,15 +4,26 @@ from django.views.generic.base import TemplateView
 from blog.models import Category, Post
 # Create your views here.
 
+def makecategory():
+    # 카테고리 구조를 딕셔너리에 담아 반환한다
+    tree ={}
+    for c1 in Category.objects.filter(level=1).reverse():
+        tree[c1]={}
+        if c1.under_list() != None:
+            for c2 in c1.under_list():
+                tree[c1][c2]= list()
+                if c2.under_list() != None:
+                    for c3 in c2.under_list():
+                        tree[c1][c2].append(c3)
+    return tree
 
 class Index(TemplateView):
     template_name ='blog/index.html'
-    fcategory = Category.objects.filter(level=1)
 
     def get_context_data(self, **kwargs):
         context = super(Index,self).get_context_data(**kwargs)
         context['post'] = Post
-        context['category'] = Category.objects.filter(level=1).reverse()
+        context['category'] = makecategory()
         return context
 
 class CategoryList(ListView):
