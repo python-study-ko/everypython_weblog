109d743bca1cd97ba0e53b940c3bf9366852c9d4
wesky93 <wesky93@gmail.com>
2016-07-05 14:16:05 +0900

    카테고리 모듈의 하위 목록 확인 코드 최적화
    @u-lis 님이 조언 해주신 것을 반영하여 
    보조함수로 객체 비어있음을 확인하던 것을 코드 한줄로 변경

--- webproject/blog/models.py
+++ webproject/blog/models.py
@@ -11,55 +11,40 @@ class Category(models.Model):
     포스트 카테고리
     level = 카테고리 단계로 총 3단계까지 있음
     name = 카테고리 명
-    under_category = 관계된 카테고리
-    카테고리 트리를 불러올떈 level = 1  카테고리를 추출후 관련된 2차 카테고리,3차 카테고리를 차레로 추출한다.
+    under_category = 관계된 카테고리 
     """
     level = models.IntegerField(choices=[(1,'1차 카테고리'),(2,'2차 카테고리'),(3,'3차 카테고')],default=1)
     name = models.CharField(max_length=20)
     under_category = models.ManyToManyField("self",symmetrical=True,blank=True)
 
-    # 하위 카테고리를 추출해주는 메소드 : 반환값의 len()이 0일경우 하위카테고리 없음 처리
+
     def under_list(self):
         """
         해당 객체의 하위 카테고리를 추출한다.
-        :return: 하위 카테고리가 없으면 Noen / 있으면 리스트에 해당객체를 담아
+        :return: 하위 카테고리가 없으면 Noen / 있으면 리스트에 해당객체를 담아줌
         """
         # 하위 카테고리 목록 초기화
         cate_list = None
 
-        # 보조 함수
-        def check_list(u_list):
-            """
-            하위 카테고리 존재여부 확인
-            :param u_list: 하위 카테고리 리스트
-            :return: 하위 카테고리가 존재하지 않은면 None값을 반환
-            """
-            if len(u_list) == 0:
-                return None
-            else:
-                return u_list
-
         # 1차 카테고리
         if self.level == 1:
-            u_list = [x for x in self.under_category.all().reverse() if x.level == 2]
-            cate_list = check_list(u_list)
-
+            cate_list = [x for x in self.under_category.all().reverse() if x.level == 2]
         # 2차 카테고리
         elif self.level == 2:
-            u_list = [x for x in self.under_category.all().reverse() if x.level == 3]
-            cate_list = check_list(u_list)
+            cate_list = [x for x in self.under_category.all().reverse() if x.level == 3]
         # 3차 카테고리 : 하위 카테고리를 가질수 없음
         else:
             cate_list = None
 
-        return cate_list
+        return cate_list if len(cate_list) != 0 else None
     """
     하위 카테고리 사용 예시
     a = Category.objects.get(id=1)
     for x in a.under_list():
-        print("하위 카테고리 {}".format(x))
+        print("{}차 카테고리 {}의 {}차 하위 카테고리 {}".format(a.level,a.name,x.level,x.name))
         if x.under_list() != None:
-            print("{}의 하위 카테고리 {}".format(x, x.under_list()))
+            for u in x.under_list():
+                print("{}차 카테고리 {}의 {}차 하위 카테고리 {}".format(x.level,x.name,u.level,u.name))
 
     """
     def __str__(self):
