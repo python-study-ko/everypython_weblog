07b7bfbbf7e86caf4744e0bf9aef3be7a3d24b28
sinsky <wesky93@gmail.com>
2016-07-09 22:58:28 +0900

    sudo문제로 가상환경이 제대로 생성되지 않는 문제 수정

--- makevenv.sh
+++ makevenv.sh
@@ -2,17 +2,19 @@
 
 echo "-------파이썬 가상환경 생성기-------"
 if [ ! -d .venv ]; then
- echo " 이전의 가상환경을 삭제하겠습니다.";
- rm -rf .venv
+echo " 이전의 가상환경을 삭제하겠습니다.";
+sudo rm -rf .venv
 fi
 
 echo "-------파이썬 가상환경 생성기-------"
 virtualenv -p python3.5 .venv
+
 . .venv/bin/activate
 
 echo "-------필수 모듈을 설치합니다-------"
 pip install -r requirments.txt
 
+deactivate
 echo "모든 작업이 완료 되었습니다. 모든 작업은 가상환경에서 실행하세"
 echo "source .venv/bin/activate 로 가상환경을 실행하고"
 echo "deactivate로 가상환경을 종료하면 됩니다"
