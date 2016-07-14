fefd1befbea934b42881fabc05b8bc74d4a6134d
sinsky <wesky93@gmail.com>
2016-07-14 22:41:08 +0900

    디버깅 툴바 추가

--- webproject/everpython/urls.py
+++ webproject/everpython/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url,include,patterns
+from django.conf import settings
 from django.contrib import admin
 
 urlpatterns = patterns('',
@@ -21,3 +22,9 @@ urlpatterns = patterns('',
     url(r'^admin/', admin.site.urls),
     url(r'^ckeditor/', include('ckeditor_uploader.urls')),
                        )
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += patterns('',
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    )
