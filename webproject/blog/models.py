e9fa9c4a9c8f63cb34b5272babe2b07ccea1459f
sinsky <wesky93@gmail.com>
2016-07-17 15:15:26 +0900

    새로운 카테고리 모델 구현
    1.  상하 관계가 하위 카테고리가 아닌 부모 카테고리를 지정하게끔 수정
    2. 부모 카테고리에 따라 자동으로 카테고리 레벨이 지정됨
    3. 부모 카테고리 선택시 1,2레벨만 선택할수 있으며 자기 자신을 선택하면 구문오류를 발생 시키도록 수정(자기 자신을 제외시키는 방법을 못찾음)

--- webproject/blog/models.py
+++ webproject/blog/models.py
@@ -3,12 +3,52 @@ from django.contrib.auth.models import User
 from ckeditor_uploader.fields import RichTextUploadingField
 from taggit.managers import TaggableManager
 from hitcount.models import HitCountMixin, HitCount
+from django.db.models.query import QuerySet
+from django.db.models import Q
+# 오류 발생용
+from django.core.exceptions import ValidationError
 
 # hticount 필드 연결용
 from django.contrib.contenttypes.fields import GenericRelation
 # Create your models here.
 
+class CategoryMixin(object):
+    """
+    카테고리 정보를 추출해준다
+    """
 
+    def root_category(self):
+        """
+        최상단 카테고리를 추출한다
+        :return:
+        """
+        return self.filter(level=1)
+
+    def under_list(self, obj):
+        """
+        하위 카테고리 목록을 추출한다
+        :param obj:
+        :return:
+        """
+        clist =[]
+        if obj.level == 3:
+            return obj
+        elif obj.level == 2:
+            clist += Category.object.get(under_category.filter(level=3))
+            return clist
+        elif obj.level == 1:
+            for c2 in obj.under_category.filter(level=2):
+                clist += Category.tree.under_list(c2)
+            return clist
+
+
+class CategoryQuerySets(QuerySet, CategoryMixin):
+    pass
+
+
+class CategoryManager(models.Manager, CategoryMixin):
+    def get_query_set(self):
+        return CategoryQuerySets(self.model, using=self._db)
 
 class Category(models.Model):
     """
@@ -17,43 +57,32 @@ class Category(models.Model):
     name = 카테고리 명
     under_category = 관계된 카테고리
     """
-    level = models.IntegerField(choices=[(1,'1차 카테고리'),(2,'2차 카테고리'),(3,'3차 카테고')],default=1)
-    name = models.CharField(max_length=20)
-    under_category = models.ManyToManyField("self",symmetrical=True,blank=True)
 
+    parent = models.ForeignKey('self',limit_choices_to=Q(level=1)|Q(level=2),on_delete=models.CASCADE,blank=True,null=True,help_text="상위 카테고리를 정해주세요,미지정시 최상위 카테고리가 됩니")
+    name = models.CharField(max_length=15,unique=True,help_text="카테고리 이름을 입력하세요")
+    level = models.IntegerField(choices=[(1, '최상위 카테고리'), (2, '2차 카테고리'), (3, '3차 카테고리')], blank=True)
+    object = models.Manager()
+    # 카테고리 매니저
+    tree = CategoryManager()
 
-    def under_list(self):
-        """
-        해당 객체의 하위 카테고리를 추출한다.
-        :return: 하위 카테고리가 없으면 Noen / 있으면 리스트에 해당객체를 담아줌
-        """
-        # 하위 카테고리 목록 초기화
-        cate_list = None
-
-        # 1차 카테고리
-        if self.level == 1:
-            cate_list = [x for x in self.under_category.all().reverse() if x.level == 2]
-        # 2차 카테고리
-        elif self.level == 2:
-            cate_list = [x for x in self.under_category.all().reverse() if x.level == 3]
-        # 3차 카테고리 : 하위 카테고리를 가질수 없음
-        else:
-            cate_list = None
-            return cate_list
-
-        return cate_list if len(cate_list) != 0 else None
-    """
-    하위 카테고리 사용 예시
-    a = Category.objects.get(id=1)
-    for x in a.under_list():
-        print("{}차 카테고리 {}의 {}차 하위 카테고리 {}".format(a.level,a.name,x.level,x.name))
-        if x.under_list() != None:
-            for u in x.under_list():
-                print("{}차 카테고리 {}의 {}차 하위 카테고리 {}".format(x.level,x.name,u.level,u.name))
-
-    """
     def __str__(self):
         return self.name
+
+    def clean(self):
+        """
+        카테고리 필드값 검증 및 자동 수정
+        """
+        # 부모 카테고리 검증
+        if self.parent.id == self.id:
+            raise ValidationError('자기자신을 상위카테고리로 지정할수 없습니다.',code='invalid')
+        # 카테고리 레벨 자동 지정
+        if not self.parent:
+            self.level = 1
+        elif self.parent.level == 1:
+            self.level = 2
+        elif self.parent.level == 2:
+            self.level = 3
+
 
 
 class Post(models.Model,HitCountMixin):
