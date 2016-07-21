5dd8db942ac8af75c88b403bbd2dddf95284e2d8
sinsky <wesky93@gmail.com>
2016-07-21 17:53:57 +0900

    배포 스크립트 수정
    settings.ini 가 없을때만 복사 하도록 변경함

--- deploy_django_project.sh
+++ deploy_django_project.sh
@@ -15,7 +15,13 @@ virtualenv -p python3.5 .venv
 echo "-------필수 모듈을 설치합니다-------"
 pip install -r requirments.txt
 
+if [ ! -f weproject/settings.ini ]
+then
+    echo "설정 파일이 없어 샘플 설정을 복사했습니다"
+    cp webproject/settings.ini.sample webproject/settings.ini
+fi
+
 deactivate
-echo "모든 작업이 완료 되었습니다. 모든 작업은 가상환경에서 실행하세"
+echo "모든 작업이 완료 되었습니다. settings.ini에서 설정을 바꾸세요"
 echo "source .venv/bin/activate 로 가상환경을 실행하고"
 echo "deactivate로 가상환경을 종료하면 됩니다"
