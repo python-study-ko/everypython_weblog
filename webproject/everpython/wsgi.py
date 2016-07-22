e1480ff24903d7a0036eddf6f3f36cdbefcc6589
sinsky <wesky93@gmail.com>
2016-07-22 10:27:12 +0900

    whitnoise 설정 제어 추가

--- webproject/everpython/wsgi.py
+++ webproject/everpython/wsgi.py
@@ -14,7 +14,15 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
+# ini파일 호출
+from configparser import RawConfigParser
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+config = RawConfigParser()
+config.read(os.path.join(BASE_DIR, 'settings.ini'))
+switch_whitnoise = config.get('deploy','WHITENOISE')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "everpython.settings")
-
-application = DjangoWhiteNoise(get_wsgi_application())
+if switch_whitnoise == 'True':
+    application = DjangoWhiteNoise(get_wsgi_application())
+elif switch_whitnoise == 'False':
+    application = get_wsgi_application()
