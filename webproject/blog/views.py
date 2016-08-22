6150c2519c82cfcf5f2eed0c74bb59cbac398272
sinsky <wesky93@gmail.com>
2016-08-22 15:48:26 +0900

    page nation 구현
    카테고리와 태그페이지에 페이지처리 기능 추가

--- webproject/blog/views.py
+++ webproject/blog/views.py
@@ -1,14 +1,16 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from django.template.loader import render_to_string
 from django.http import HttpResponse
 from django.views.generic import View
 from .models import Category, Post
 from django.conf import settings
 from django.db.models import Count
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from hitcount.models import HitCount
 from taggit.models import Tag
 from hitcount.views import HitCountDetailView
+
 
 def sidebar_context():
     """
@@ -16,9 +18,9 @@ def sidebar_context():
     context.update(sidebar_context())이걸 통해 각페이지 context에 카테고리나 태그정보가 담긴 context를 함께 넘겨준다
     :return:
     """
-    tags = Tag.objects.all().filter(post__publish=True).annotate(Count('post')).values_list('name','post__count')
+    tags = Tag.objects.all().filter(post__publish=True).annotate(Count('post')).values_list('name', 'post__count')
     categorytree = Category.tree.navi_bar()
-    context_dic={'tags':tags,'categorytree':categorytree}
+    context_dic = {'tags': tags, 'categorytree': categorytree}
     return context_dic
 
 
@@ -42,48 +44,65 @@ class Index(View):
         """
 
         # 사이드바에 필요한 context를 합쳐줌
-        context = {'newposts':newposts,'starposts':starposts}
+        context = {'newposts': newposts, 'starposts': starposts}
         context.update(sidebar_context())
         data = render_to_string("blog/index.html", context, request=request)
         return HttpResponse(data)
 
 
-def CategoryList(request,name):
-    c = get_object_or_404(Category,name=name)
+def CategoryList(request, name):
+    c = get_object_or_404(Category, name=name)
     categorys = Category.tree.under_list(c)
     if c.level == 3:
         post_queryset = Post.published.publish().filter(category=c).order_by('-id')
     else:
         categorys = Category.tree.under_list(c)
         post_queryset = Post.published.publish().filter(category__in=categorys).order_by('-id')
-    posts = Post.published.posts_info(post_queryset)
-    # 포스트 목록
-    context = {"posts": posts,"name":c.name}
+    post_list = Post.published.posts_info(post_queryset)
+    page = request.GET.get('page')
+
+    # 포스트 목록 페이지 처리
+    paginator = Paginator(post_list, 8)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        posts = paginator.page(paginator.num_pages)
+
+    context = {"posts": posts, "name": c.name}
     # 사이드바에 필요한 context를 합쳐줌
     context.update(sidebar_context())
-    return render(request,'blog/category.html',context)
+    return render(request, 'blog/category.html', context)
 
-def TagList(request,name):
+
+def TagList(request, name):
     tag = Tag.objects.get(name=name)
     # 태그에 속한 모든 포스트를 찾는다
     post_queryset = Post.published.publish().filter(tag__name__in=[tag]).order_by('-id')
-    posts = Post.published.posts_info(post_queryset)
-    context = {"posts": posts,"name":tag.name}
+    post_list = Post.published.posts_info(post_queryset)
+
+    context = {"posts": posts, "name": tag.name}
     # 사이드바에 필요한 context를 합쳐줌
     context.update(sidebar_context())
-    return render(request,'blog/tag.html',context)
+    return render(request, 'blog/tag.html', context)
+
 
 class PostDetail(HitCountDetailView):
     model = Post
     count_hit = True
+
     def get_context_data(self, **kwargs):
         context = super(PostDetail, self).get_context_data(**kwargs)
         # 포스트 태그 리스트를 생성
-        tags = Post.objects.get(pk=self.kwargs.get('pk')).tag.get_queryset().annotate(Count('post')).values_list('id','name','post__count')
+        tags = Post.objects.get(pk=self.kwargs.get('pk')).tag.get_queryset().annotate(Count('post')).values_list('id',
+                                                                                                                 'name',
+                                                                                                                 'post__count')
         context['posttag'] = tags
-        context['shortname'] = getattr(settings,'SHORTNAME')
+        context['shortname'] = getattr(settings, 'SHORTNAME')
         # 사이드바에 필요한 context를 합쳐줌
         context.update(sidebar_context())
         return context
-
-
